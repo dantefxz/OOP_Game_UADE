6292e7347b23007cd7731390e2b69d4cbf050dd2
Latis1000 <lautaroabate2005@gmail.com>
2025-06-21 17:52:29 -0300

    N/A

--- src/Characters/Player/Cthulhu.java
+++ src/Characters/Player/Cthulhu.java
@@ -3,9 +3,9 @@ package Characters.Player;
 import Characters.BaseCharacter;
 import Misc.AttackManager;
 
-public class Dante extends BaseCharacter {
+public class Cthulhu extends BaseCharacter {
 
-    public Dante() {
+    public Cthulhu() {
         super("Dante", 300);
         new AttackManager("Base_Atack", 50, 10, 15, 0);
         new AttackManager("Ability", 30, 20,20, 1);
